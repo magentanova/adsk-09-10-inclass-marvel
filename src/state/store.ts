801c7b767d2f530cd782442a0ca34bd63df70cd2
Justin Richards <richards.justind@gmail.com>
2019-03-25 23:10:50 -0400

    cleaned out repo

--- src/state/store.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers, createStore } from 'redux';
-
-import * as reducers from './reducers';
-
-export const store = createStore(combineReducers({...reducers}));
-
-export const dispatch = store.dispatch.bind(store);
